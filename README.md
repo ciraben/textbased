bc91436bc2dcbddbf45d6bb148edaaf281dfa8f3
ciraben <tom.on.github@gmail.com>
2023-03-17 22:25:07 -0700

    Generate package scaffolding.

---
+++ README.md
@@ -0,0 +1,3 @@
+# textbased
+
+A package to supplement making textbased CLI games.
---
+++ textbased/__init__.py
@@ -0,0 +1,6 @@
+# add import lines like so - one for each module
+# makes import more intuitive for end-user
+# allows them to access modules afterwards, like:
+#   import textbased
+#   x = textbased.foo.func()
+import textbased.foo as foo
